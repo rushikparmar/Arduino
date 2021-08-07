701ac6f98f25fdc1e514b8ce4b5d9e713392ec54
rushikparmar <85787166+rushikparmar@users.noreply.github.com>
2021-08-07 20:22:04 +0530

    Create Read_Data_From_URL_ESP8266.ino

---
+++ Read_Data_From_URL_ESP8266.ino
@@ -0,0 +1,79 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "Vibe";
+const char* password = "987654321.";
+const char* host = "rushik-parmar.000webhostapp.com";
+
+String line;
+char data[20];
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(10);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+  }
+  
+  Serial.println(WiFi.localIP());
+}
+
+void loop() 
+{
+  int i=0,j=0;
+  
+  delay(1000);
+  
+  WiFiClient client;
+  const int httpPort = 80;
+  
+  if (!client.connect(host, httpPort)) 
+  {
+    return;
+  }
+  
+  String url = "/b.php/";
+  
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  
+  unsigned long timeout = millis();
+  
+  while (client.available() == 0) 
+  {
+    if (millis() - timeout > 5000) 
+    {
+      client.stop();
+      return;
+    }
+  } 
+  
+  while(client.available())
+  {
+    line = client.readStringUntil('\r');
+    char d[line.length()];
+    line.toCharArray(d,line.length());
+    
+    for(i=0;i<line.length();i++)
+    {
+      if(d[i]=='*')
+      {
+        break;
+      }
+    }
+  
+    for(i=i;i<line.length();i++)
+    {
+      data[j]=d[i];
+      j++;
+    }
+  }
+   
+  Serial.println(data);
+}
+
